8a3d6a155d0ca3b333edacbe9112e69b52644650
Coconutat <coconutat@protonmail.com>
2023-03-31 12:05:50 +0800

    Clean some file

--- android_kernel_huawei_hi3660/build_KSU.sh
+++ android_kernel_huawei_hi3660/build_KSU.sh
@@ -64,6 +64,7 @@ make ARCH=arm64 O=out $EV -j128
 if [ -f out/arch/arm64/boot/Image.gz ];
 then
 	echo "***Packing P10 version kernel...***"
+	tools/mkbootimg --kernel out/arch/arm64/boot/Image.gz --base 0x0 --cmdline "loglevel=4 initcall_debug=n page_tracker=on slub_min_objects=16 unmovable_isolate1=2:192M,3:224M,4:256M printktimer=0xfff0a000,0x534,0x538 androidboot.selinux=enforcing buildvariant=user" --tags_offset 0x07A00000 --kernel_offset 0x00080000 --ramdisk_offset 0x07c00000 --header_version 1 --os_version 9 --os_patch_level 2020-09-05  --output PK_V"$v"_9.0_P10.img
 	tools/mkbootimg --kernel out/arch/arm64/boot/Image.gz --base 0x0 --cmdline "loglevel=4 initcall_debug=n page_tracker=on slub_min_objects=16 unmovable_isolate1=2:192M,3:224M,4:256M printktimer=0xfff0a000,0x534,0x538 androidboot.selinux=permissive buildvariant=user" --tags_offset 0x07A00000 --kernel_offset 0x00080000 --ramdisk_offset 0x07c00000 --header_version 1 --os_version 9 --os_patch_level 2020-09-05  --output PK_V"$v"_9.0_P10_PM.img
 	cp out/arch/arm64/boot/Image.gz tools/AnyKernel3/Image.gz
 	cd tools/AnyKernel3
--- android_kernel_huawei_hi3660/security/selinux/selinuxfs.c
+++ android_kernel_huawei_hi3660/security/selinux/selinuxfs.c
@@ -164,7 +164,7 @@ static ssize_t sel_write_enforce(struct file *file, const char __user *buf,
 		goto out;
 
 		/*Modify for Huawei.This value for enforce the permissive mode.*/
-		new_value = 0;
+		/*new_value = 0;*/
 
 	if (new_value != selinux_enforcing) {
 		length = task_has_security(current, SECURITY__SETENFORCE);
