95767ca41ce6005a153361d56e0831658a6ac069
Coconut <coconutat@protonmail.com>
2023-05-16 18:17:16 +0800

    Use CONFIG_SELINUX_EPM to Fix EMUI 9 SElinux issuse

--- security/selinux/selinuxfs.c
+++ security/selinux/selinuxfs.c
@@ -158,8 +158,11 @@ static ssize_t sel_write_enforce(struct file *file, const char __user *buf,
   if (sscanf(page, "%d", &new_value) != 1)
     goto out;
 
+#ifdef CONFIG_SELINUX_EPM (ture)
   /*Modify for Huawei.This value for enforce the permissive mode.*/
-  /*new_value = 0;*/
+  new_value = 0;
+#else
+#endif
 
   if (new_value != selinux_enforcing)
   {
