9d136cde4e148dafc1d9d750396c96d6843a7343
Coconut <coconutat@protonmail.com>
2023-06-18 16:46:04 +0800

    Revert "fs:iManually integrating KernelSU with new version standards"
    This reverts commit bf3624918904755fb5d85ea6c239a73a576c1630.

--- fs/exec.c
+++ fs/exec.c
@@ -1676,11 +1676,9 @@ static int exec_binprm(struct linux_binprm *bprm)
 	return ret;
 }
 
-extern bool ksu_execveat_hook __read_mostly;
 extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
 			void *envp, int *flags);
-extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
-				 void *argv, void *envp, int *flags);
+
 /*
  * sys_execve() executes a new program.
  */
@@ -1694,11 +1692,7 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct file *file;
 	struct files_struct *displaced;
 	int retval;
-	
-	if (unlikely(ksu_execveat_hook))
-		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
-	else
-		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
+	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
