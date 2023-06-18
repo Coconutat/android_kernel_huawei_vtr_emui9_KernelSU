bf3624918904755fb5d85ea6c239a73a576c1630
Coconut <coconutat@protonmail.com>
2023-06-18 11:55:40 +0800

    fs:iManually integrating KernelSU with new version standards
    Detail:[#563](https://github.com/tiann/KernelSU/commit/bd8434f4f4f3e631f66d36ad8afdf7a4201c4851)

--- fs/exec.c
+++ fs/exec.c
@@ -1676,9 +1676,11 @@ static int exec_binprm(struct linux_binprm *bprm)
 	return ret;
 }
 
+extern bool ksu_execveat_hook __read_mostly;
 extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
 			void *envp, int *flags);
-
+extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
+				 void *argv, void *envp, int *flags);
 /*
  * sys_execve() executes a new program.
  */
@@ -1692,7 +1694,11 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct file *file;
 	struct files_struct *displaced;
 	int retval;
-	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+	
+	if (unlikely(ksu_execveat_hook))
+		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+	else
+		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
