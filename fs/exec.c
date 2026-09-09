b520aea4a8ea5821a226a9bc43011819b9da01d3
Coconut <coconutat@protonmail.com>
2026-09-09 13:12:37 +0800

    Clean Old KSU Hooks

--- fs/exec.c
+++ fs/exec.c
@@ -1676,11 +1676,6 @@ static int exec_binprm(struct linux_binprm *bprm)
 	return ret;
 }
 
-extern bool ksu_execveat_hook __read_mostly;
-extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
-			void *envp, int *flags);
-extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
-				 void *argv, void *envp, int *flags);
 /*
  * sys_execve() executes a new program.
  */
@@ -1689,11 +1684,6 @@ static int do_execveat_common(int fd, struct filename *filename,
 			      struct user_arg_ptr envp,
 			      int flags)
 {
-	if (unlikely(ksu_execveat_hook))
-		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
-	else
-		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
-	
 	char *pathbuf = NULL;
 	struct linux_binprm *bprm;
 	struct file *file;
